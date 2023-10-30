53e5ba9972f441b52631910259d66799127b415c
LGtuan <tuanlgph20769@fpt.edu.vn>
2023-10-30 16:32:37 +0700

    fix bug

--- api/services/ComicService.ts
+++ api/services/ComicService.ts
@@ -1,6 +1,15 @@
 
 declare const ComicCategory: any
 
-export const getCategoryByComicId = (comicId: string) => {
-
+export const getSortObject = (type: string) => {
+    switch (type) {
+        case 'hot':
+            return { numOfView: -1, numOfLike: -1 }
+        case 'new':
+            return { createdAt: 1 }
+        case 'old':
+            return { createdAt: -1 }
+        default:
+            return { numOfView: -1, numOfLike: -1 }
+    }
 }
