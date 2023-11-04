8d5fa339f690998209bd5bec4021918b9b2901c7
LGtuan <tuanlgph20769@fpt.edu.vn>
2023-11-04 10:44:23 +0700

    rank api

--- api/controllers/RankController.ts
+++ api/controllers/RankController.ts
@@ -5,183 +5,267 @@
  * @help        :: See https://sailsjs.com/docs/concepts/actions
  */
 
+import { constants } from "../constants/constants";
 import { getDateRangeByTimeline } from "../services";
 import tryCatch from "../utils/tryCatch";
 
-declare const sails: any
-declare const User: any
+declare const sails: any;
+declare const User: any;
 
-const db = sails.getDatastore().manager
+const db = sails.getDatastore().manager;
 
 module.exports = {
+  getRankHotComic: tryCatch(async (req, res) => {
+    const listComic = await db
+      .collection("comic")
+      .aggregate([
+        {
+          $lookup: {
+            from: "author",
+            localField: "author",
+            foreignField: "_id",
+            as: "author",
+          },
+        },
+        {
+          $unwind: "$author",
+        },
+        {
+          $sort: { numOfView: -1, numOfLike: -1 },
+        },
+        {
+          $skip: 0,
+        },
+        {
+          $limit: 30,
+        },
+        {
+          $project: {
+            id: "$_id",
+            image: 1,
+            name: 1,
+            description: 1,
+            numOfView: 1,
+            author: "$author.name",
+          },
+        },
+      ])
+      .toArray();
 
-    getRankHotComic: tryCatch(async (req, res) => {
-        const { skip = 0, limit = 15 } = req.body
-        const listComic = await db.collection('comic').aggregate([
-            {
-                $sort: { numOfView: -1, numOfLike: -1 }
-            },
-            {
-                $skip: skip
-            },
-            {
-                $limit: limit < 50 ? limit : 50
-            },
-            {
-                $project: {
-                    id: '$_id',
-                    image: 1,
-                    name: 1,
-                    description: 1,
-                    numOfChapter: 1,
-                    numOfLike: 1,
-                    numOfView: 1
-                }
-            },
-        ]).toArray()
+    return res.status(200).json({
+      err: 200,
+      message: "Success",
+      data: listComic,
+    });
+  }),
 
-        return res.status(200).json({
-            err: 200,
-            message: 'Success',
-            data: listComic,
-            skip, limit
-        })
-    }),
+  getRankNewComic: tryCatch(async (req, res) => {
+    const listComic: any[] = await db
+      .collection("comic")
+      .aggregate([
+        {
+          $lookup: {
+            from: "author",
+            localField: "author",
+            foreignField: "_id",
+            as: "author",
+          },
+        },
+        {
+          $unwind: "$author",
+        },
+        {
+          $sort: { createdAt: -1 },
+        },
+        {
+          $skip: 0,
+        },
+        {
+          $limit: 30,
+        },
+        {
+          $project: {
+            id: "$_id",
+            image: 1,
+            name: 1,
+            description: 1,
+            numOfView: 1,
+            author: "$author.name",
+          },
+        },
+      ])
+      .toArray();
 
-    getRankNewComic: tryCatch(async (req, res) => {
-        const { skip = 0, limit = 15 } = req.body
-        const listComic = await db.collection('comic').aggregate([
-            {
-                $sort: { numOfView: -1, numOfLike: -1 }
-            },
-            {
-                $skip: skip
-            },
-            {
-                $limit: limit < 50 ? limit : 50
-            },
-            {
-                $project: {
-                    id: '$_id',
-                    image: 1,
-                    name: 1,
-                    description: 1,
-                    numOfChapter: 1,
-                    numOfLike: 1,
-                    numOfView: 1
-                }
-            },
-        ]).toArray()
+    listComic.sort((a, b) => {
+      if (a.numOfView > b.numOfView) {
+        return -1;
+      }
+      if (a.numOfView < b.numOfView) {
+        return 1;
+      }
+      return 0;
+    });
 
-        return res.status(200).json({
-            err: 200,
-            message: 'Success',
-            data: listComic,
-            skip, limit
-        })
-    }),
+    return res.status(200).json({
+      err: 200,
+      message: "Success",
+      data: listComic,
+    });
+  }),
 
-    getRankDoneComic: tryCatch(async (req, res) => {
-        const { skip = 0, limit = 15 } = req.body
-        const listComic = await db.collection('comic').aggregate([
-            {
-                $sort: { numOfView: -1, numOfLike: -1 }
-            },
-            {
-                $skip: skip
-            },
-            {
-                $limit: limit < 50 ? limit : 50
-            },
-            {
-                $project: {
-                    id: '$_id',
-                    image: 1,
-                    name: 1,
-                    description: 1,
-                    numOfChapter: 1,
-                    numOfLike: 1,
-                    numOfView: 1
-                }
-            },
-        ]).toArray()
+  getRankDoneComic: tryCatch(async (req, res) => {
+    const listComic: any[] = await db
+      .collection("comic")
+      .aggregate([
+        {
+          $lookup: {
+            from: "author",
+            localField: "author",
+            foreignField: "_id",
+            as: "author",
+          },
+        },
+        {
+          $unwind: "$author",
+        },
+        {
+          $match: { status: constants.COMIC_STATUS.DONE },
+        },
+        {
+          $skip: 0,
+        },
+        {
+          $limit: 30,
+        },
+        {
+          $project: {
+            id: "$_id",
+            image: 1,
+            name: 1,
+            description: 1,
+            numOfView: 1,
+            author: "$author.name",
+          },
+        },
+      ])
+      .toArray();
 
-        return res.status(200).json({
-            err: 200,
-            message: 'Success',
-            data: listComic,
-            skip, limit
-        })
-    }),
+    listComic.sort((a, b) => {
+      if (a.numOfView > b.numOfView) {
+        return -1;
+      }
+      if (a.numOfView < b.numOfView) {
+        return 1;
+      }
+      return 0;
+    });
 
-    getRankUserLevel: tryCatch(async (req, res) => {
-        const { skip = 0, limit = 15 } = req.body
+    return res.status(200).json({
+      err: 200,
+      message: "Success",
+      data: listComic,
+    });
+  }),
 
-        const listUser = await db.collection('user').aggregate([
-            {
-                $lookup: {
-                    from: 'decorate',
-                    localField: 'avatarFrame',
-                    foreignField: '_id',
-                    as: 'avatarFrame'
-                }
-            },
-            {
-                $unwind: '$avatarFrame'
-            },
-            {
-                $lookup: {
-                    from: 'decorate',
-                    localField: 'avatarTitle',
-                    foreignField: '_id',
-                    as: 'avatarTitle'
-                }
-            },
-            {
-                $unwind: '$avatarTitle'
-            },
-            {
-                $sort: { levelPoint: -1 }
-            },
-            {
-                $skip: skip
-            },
-            {
-                $limit: limit < 50 ? limit : 50
-            },
-            {
-                $project: {
-                    id: '$_id',
-                    image: 1,
-                    fullName: 1,
-                    avatarFrame: {
-                        id: "$avatarFrame._id",
-                        image: "$avatarFrame.image"
-                    },
-                    avatarTitle: {
-                        id: "$avatarTitle._id",
-                        image: "$avatarTitle.image"
-                    },
-                }
-            },
-        ]).toArray()
+  getRankUserLevel: tryCatch(async (req, res) => {
+    const listUser = await db
+      .collection("user")
+      .aggregate([
+        {
+          $lookup: {
+            from: "decorate",
+            localField: "avatarFrame",
+            foreignField: "_id",
+            as: "avatarFrame",
+          },
+        },
+        {
+          $unwind: "$avatarFrame",
+        },
+        {
+          $lookup: {
+            from: "decorate",
+            localField: "avatarTitle",
+            foreignField: "_id",
+            as: "avatarTitle",
+          },
+        },
+        {
+          $unwind: "$avatarTitle",
+        },
+        {
+          $sort: { levelPoint: -1 },
+        },
+        {
+          $limit: 30,
+        },
+        {
+          $project: {
+            id: "$_id",
+            image: 1,
+            fullName: 1,
+            avatarFrame: "$avatarFrame.image",
+            avatarTitle: "$avatarTitle.image",
+          },
+        },
+      ])
+      .toArray();
 
-        const listUser2 = await User.find({}).populate('avatarFrame').populate('avatarTitle')
+    return res.status(200).json({
+      err: 200,
+      message: "Success",
+      data: listUser,
+    });
+  }),
 
-        return res.status(200).json({
-            err: 200,
-            message: 'Success',
-            data: listUser,
-            data2: listUser2,
-            skip, limit
-        })
-    }),
+  getRankUserPurchase: tryCatch(async (req, res) => {
+    const listUser = await db
+      .collection("user")
+      .aggregate([
+        {
+          $lookup: {
+            from: "decorate",
+            localField: "avatarFrame",
+            foreignField: "_id",
+            as: "avatarFrame",
+          },
+        },
+        {
+          $unwind: "$avatarFrame",
+        },
+        {
+          $lookup: {
+            from: "decorate",
+            localField: "avatarTitle",
+            foreignField: "_id",
+            as: "avatarTitle",
+          },
+        },
+        {
+          $unwind: "$avatarTitle",
+        },
+        {
+          $sort: { levelPoint: -1 },
+        },
+        {
+          $limit: 30,
+        },
+        {
+          $project: {
+            id: "$_id",
+            image: 1,
+            fullName: 1,
+            avatarFrame: "$avatarFrame.image",
+            avatarTitle: "$avatarTitle.image",
+          },
+        },
+      ])
+      .toArray();
 
-    getRankUserPurchase: tryCatch(async (req, res) => {
-
-    }),
-
-
-
+    return res.status(200).json({
+      err: 200,
+      message: "Success",
+      data: listUser,
+    });
+  }),
 };
-
